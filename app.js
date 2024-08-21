3675cda90adad0e7ae28793930066643421f6729
jennyjonesigwe <jennyjonesigwe@gmail.com>
2024-08-21 12:07:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var school = "Ebonyi State University"
+var state = "Abakaliki"
+var name = "Jenny Jones Igwe"
+
+console.log(school)
+console.log(state)
+console.log(name)
+
+let course = "JavaScript"
+let age = "40"
+
+console.log(course)
+console.log(age)
+
+const topic = "Variables is our topic and we are looking into it"
+const year = 2024
+
+console.log(topic)
+console.log(year)
